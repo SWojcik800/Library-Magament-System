49a014399357df27f614f9a3cc0848eb13856015
unknown <leonjasnoszumi78@gmail.com>
2019-08-03 16:04:04 -0700

    Initial commit on this branch

--- LMS_backend/LMS_backend/settings.py
+++ LMS_backend/LMS_backend/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
+    'CRUD.apps.CrudConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
