cbef9e72886a13cff113bb0e92eb6c361a46df25
LDVES <leonjasnoszumi78@gmail.com>
2019-08-04 11:43:56 -0700

    Allowed CORS Headers for All

--- LMS_backend/LMS_backend/settings.py
+++ LMS_backend/LMS_backend/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS Headers Allowed for All
+CORS_ORIGIN_ALLOW_ALL = True
